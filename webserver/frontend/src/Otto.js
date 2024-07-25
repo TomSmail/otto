20d2e0b0f34a50cab0d14e04277ce790d578e76f
TomSmail <thomas.c.smail@gmail.com>
2024-07-25 23:28:14 +0100

    feat: add otto assistant face

---
+++ webserver/frontend/src/Otto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+import './index.css';
+
+ const Otto = () => {
+    return (
+        <div className='flex-container'>
+            <Spline scene="https://prod.spline.design/OY5NNsHPx5VQHeCr/scene.splinecode" />
+        </div>
+      );
+};
+export default Otto;
---
+++ webserver/frontend/src/components/TextChat.js
@@ -0,0 +1,10 @@
+import React from  'react';
+import './index.css';
+
+const TextChat = () => {
+    return (
+        <div className='container'>
+            <h1>Text Chat</h1>
+        </div>
+    )
+}
